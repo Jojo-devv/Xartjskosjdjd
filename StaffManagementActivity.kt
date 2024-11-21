6cb4efe65090d1c9aeb8bad5e835e050af06ec21
Jojo <joywriter271@gmail.com>
2024-11-21 21:52:00 +0300

    Update

---
+++ StaffManagementActivity.kt
@@ -0,0 +1,12 @@
+package com.yogastudio.admin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StaffManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_management)
+        // Implementation for managing staff
+    }
+}
