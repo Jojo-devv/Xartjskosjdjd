444d7a3dc1a23344142b9771c76ed4350b24f8ca
Jojo <joywriter271@gmail.com>
2024-11-21 21:58:43 +0300

    Update

---
+++ ClassesPage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace YogaStudioApp
+{
+    public partial class ClassesPage : ContentPage
+    {
+        public ClassesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
